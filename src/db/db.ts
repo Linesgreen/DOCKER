0a2df409d32891698ab2dfb7f89170669417069b
Vladislav Evg <linesgreen@mail.ru>
2023-11-24 15:05:44 +0300

    add mongo uri

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,11 +1,15 @@
+import dotenv from 'dotenv'
 import {Collection, MongoClient} from "mongodb";
 import {BlogType} from "../types/blogs/output";
 import {PostType} from "../types/posts/output";
 
+dotenv.config()
 export const port = process.env.PORT || 5000;
 
 console.log(process.env.PORT)
+
 const mongoUri : string = process.env.MONGO_URL || 'mongodb://0.0.0.0:27017'
+
 console.log(process.env.MONGO_URL)
 
 const client = new MongoClient(mongoUri)
